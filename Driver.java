0328d553adb5a134a0fc342ba634f3a183f6cfd3
wxhwxh2003 <wxhwxhwxh2003@gmail.com>
2022-10-03 20:09:16 -0400

    modified Polynomial.java and Driver.java

--- Driver.java
+++ Driver.java
@@ -1,16 +1,74 @@
-public class Driver {
-public static void main(String [] args) {
-Polynomial p = new Polynomial();
-System.out.println(p.evaluate(3));
-double [] c1 = {6,0,0,5};
-Polynomial p1 = new Polynomial(c1);
-double [] c2 = {0,-2,0,0,-9};
-Polynomial p2 = new Polynomial(c2);
-Polynomial s = p1.add(p2);
-System.out.println("s(0.1) = " + s.evaluate(0.1));
-if(s.hasRoot(1))
-System.out.println("1 is a root of s");
-else
-System.out.println("1 is not a root of s");
-}
+import lab1.Polynomial;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Polynomial p = new Polynomial();
+        //  System.out.println(p.evaluate(3));
+//        double [] c1 = {6,5};
+//        int [] c2 = {0,3};
+        double [] c1 = {6,-4,5};
+        int [] c2 = {0,1,3};
+        Polynomial p1 = new Polynomial(c1,c2);
+        double [] d1 = {-2,-9};
+        int [] d2 = {1,4};
+        Polynomial p2 = new Polynomial(d1,d2);
+        Polynomial s1 = p1.add(p2);
+        Polynomial s2 = p1.multiply(p2);
+
+//        System.out.println("The length is "+s.array.length);
+//        for (int i=0; i<s.exp.length; i++)
+//            System.out.println(s.exp[i]);
+//        for (int i=0; i<s.array.length; i++)
+//            System.out.println(s.array[i]);
+
+        System.out.println("s1(0) = " + s1.evaluate(0));
+        System.out.println("s1(1) = " + s1.evaluate(1));
+        if(s1.hasRoot(1))
+            System.out.println("1 is a root of s2");
+        else
+            System.out.println("1 is not a root of s2");
+
+        System.out.println("s2(0) = " + s2.evaluate(0));
+        System.out.println("s2(1) = " + s2.evaluate(1));
+        if(s2.hasRoot(1))
+            System.out.println("1 is a root of s2");
+        else
+            System.out.println("1 is not a root of s2");
+//
+//        File file = new File("C:/UTSC/2nd year/CSCB07/lab2.txt");
+//        Scanner scan = new Scanner(file);
+
+        Polynomial po = new Polynomial(c1, c2);
+
+//        for (int i=0; i<po.exp.length; i++){
+//            System.out.println("Coe "+i+ " is "+po.array[i]);
+//            System.out.println("Exp "+i+ " is "+po.exp[i]);
+//        }
+
+        po.saveToFile("function.txt");
+
+//        String content = scan.nextLine().replace("-", "+-");
+//        content = content.substring(1,content.length());
+//        String [] lines = content.split("\\+");
+//        for (String f:lines) System.out.println(f);
+//         System.out.println(content);
+//         System.out.println("lines length is "+ lines.length);
+//        String function = "";
+//        for (int i=0; i<c1.length; i++){
+//            if (c2[i] == 0) function = function+(int)c1[i] + '+';
+//            else {
+//                function = function  +(int)c1[i] + 'x' + c2[i] + '+';
+//            }
+//        }
+//        function = function.substring(0, function.length()-1);
+//      function = function.replace("+-", "-");
+//        System.out.println(function);
+
+
+    }
 }
--- Polynomial.java
+++ Polynomial.java
@@ -1,47 +1,182 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Polynomial {
     double [] array;
+    int [] exp;
 
     public Polynomial(){
         this.array = new double [0];
+        this.exp = new int [0];
     }
-    public Polynomial(double [] arr){
+    public Polynomial(double [] arr, int [] ex){
+        this.exp = ex;
         this.array = arr;
     }
 
-    public Polynomial add(Polynomial poly) {
-        Polynomial max;
-        Polynomial min;
-        if (this.array.length < poly.array.length) {
-            max = poly;
-            min = this;
-        }
-        else {
-            max = this;
-            min = poly;
-        }
+    public Polynomial (File file) throws IOException {
+        Scanner scan = new Scanner(file);
+        String str = scan.nextLine().replace("-", "+-");
+        scan.close();
+        if (str.charAt(0) == '+') str = str.substring(1, str.length());
 
-        double [] arr = new double[max.array.length];
-        Polynomial p = new Polynomial(arr);
-       // System.out.println("Max array length is: "+max.array.length);
+        String [] lines = str.split("\\+");
+        int len = lines.length;
+        double [] coe = new double [len];
+        int [] ex = new int [len];
 
-        for (int i = 0; i<max.array.length; i++){
-            if (i<min.array.length) {
-                p.array[i] = min.array[i] + max.array[i];
+        for (int i=0; i< len; i++){
+            int idx = lines[i].indexOf('x');
+            if (idx == -1){
+                coe[i] = Integer.parseInt(lines[i]);
+                ex[i] = 0;
             }
-            else p.array[i] = max.array[i];
+            else {
+                coe[i] = Integer.parseInt(lines[i].substring(0,idx));
+                ex[i] = Integer.parseInt(lines[i].substring(idx+1));
+            }
         }
+        this.array = coe;
+        this.exp = ex;
+    }
+
+    public int dup (int [] arr){
+        int total = 0;
+        for (int i=0; i<arr.length-1; i++){
+            for (int j=i+1; j<arr.length; j++){
+                if (arr[i] == arr[j]) total++;
+            }
+        }
+        return total;
+    }
+
+    public lab1.Polynomial sort (double [] coe, int [] exp){
+        double [] c = coe;
+        int [] e = exp;
+        int len = coe.length;
+
+        for (int i=0; i<len; i++){
+            int smallest = i;
+            for (int j=i+1; j<len; j++){
+                if (exp[j] < exp[smallest])  smallest = j;
+            }
+            if (exp[smallest] < exp[i]){
+                int tmp1 = exp[smallest];
+                double tmp2 = coe[smallest];
+                exp[smallest] = exp[i];
+                coe[smallest] = coe[i];
+                exp[i] = tmp1;
+                coe[i] = tmp2;
+            }
+        }
+        lab1.Polynomial p = new lab1.Polynomial(c,e);
+        return p;
+    }
+    public double [] convert(double [] coe, int [] ex, int len){
+        double [] a = new double [len];
+        for (int i=0; i<ex.length; i++)
+            a[ex[i]] = coe[i];
+
+        return a;
+    }
+    public lab1.Polynomial add(lab1.Polynomial poly) {
+        int [] new_array = new int [this.exp.length+poly.exp.length];
+        for (int i=0; i<this.exp.length; i++) new_array[i] = this.exp[i];
+        for (int i=this.exp.length; i< new_array.length; i++) new_array[i] = poly.exp[i- exp.length];
+        // int len = this.exp.length+ poly.exp.length - dup(this.exp, poly.exp);
+        int len = this.exp.length+ poly.exp.length - dup(new_array);
+        int len1 = Math.max(this.exp[this.exp.length-1], poly.exp[poly.exp.length-1])+1;
+        double [] coe = new double[len];
+        int [] ex = new int [len];
+        double [] a = convert(this.array, this.exp, len1);
+        double [] b = convert(poly.array, poly.exp, len1);
+        double [] ab = new double [Math.max(this.exp[this.exp.length-1], poly.exp[poly.exp.length-1])+1];
+
+        for (int i=0; i<ab.length; i++)
+            ab[i] = a[i] + b[i];
+
+        int index = 0;
+        for (int i=0; i<ab.length; i++){
+            if (ab[i]!=0){
+                coe[index] = ab[i];
+                ex[index] = i;
+                index++;
+            }
+        }
+
+        lab1.Polynomial p = new lab1.Polynomial(coe, ex);
         return p;
     }
 
+    public lab1.Polynomial multiply (lab1.Polynomial poly){
+        int len = this.exp.length*poly.exp.length;
+
+        int [] merge_exp = new int [len];
+        double [] merge_coe = new double [len];
+        for (int i=0; i<this.exp.length;i++){
+            for (int j=0; j<poly.exp.length; j++){
+                int index = i*poly.exp.length+j;
+                merge_exp[index] = this.exp[i]+poly.exp[j];
+                merge_coe[index] = this.array[i]*poly.array[j];
+            }
+        }
+
+        int [] real_exp = new int [len-dup(merge_exp)] ;
+        double [] real_coe = new double [len-dup(merge_exp)] ;
+
+        for (int i=0; i<merge_exp.length-1; i++){
+            for (int j=i+1; j<merge_exp.length; j++){
+                if ( merge_exp[i] != 0 && merge_exp[i] == merge_exp[j]) {
+                    merge_exp[j] = 0;
+                    merge_coe[i]+=merge_coe[j];
+                    merge_coe[j] = 0;
+                }
+            }
+        }
+
+        int idx = 0;
+        for (int i=0; i<merge_exp.length; i++){
+            if (merge_exp[i] != 0){
+                real_exp[idx] = merge_exp[i];
+                real_coe[idx] = merge_coe[i];
+                idx++;
+            }
+        }
+
+        lab1.Polynomial p = sort(real_coe, real_exp);
+        return p;
+    }
     public double evaluate(double x){
         double res = 0;
         for (int i=0; i<this.array.length;i++){
-            res += this.array[i]*Math.pow(x, i);
+            res += this.array[i]*Math.pow(x, this.exp[i]);
         }
         return res;
     }
 
     public boolean hasRoot(double x){
         return evaluate(x) == 0;
+    }
+
+    public void saveToFile(String name) throws IOException {
+        FileWriter write = new FileWriter(name);
+        String function = "";
+
+        for (int i=0; i<this.exp.length; i++){
+            if (this.exp[i] == 0) function = function+(int)this.array[i] + '+';
+            else {
+                function = function  +(int)this.array[i] + 'x' + this.exp[i] + '+';
+            }
+        }
+        function = function.substring(0, function.length()-1);
+        function = function.replace("+-", "-");
+
+        // System.out.println("file is "+function);
+
+        write.write(function);
+        write.close();
     }
 }
